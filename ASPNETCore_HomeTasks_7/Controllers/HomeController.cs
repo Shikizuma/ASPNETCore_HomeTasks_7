0a4476576b07472880340c8064794afe1af771ce
Shikizuma <myroslav.bashkirov@gmail.com>
2024-02-16 16:46:04 +0200

    Add project files.

---
+++ ASPNETCore_HomeTasks_7/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ASPNETCore_HomeTasks_7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace ASPNETCore_HomeTasks_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {   
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PersonModel model)
+        {
+            //Console.WriteLine($"First: {model.First}, Second: {model.Second}, Count: {model.Count}");
+            System.Diagnostics.Debug.WriteLine($"First: {model.First}, Second: {model.Second}, Count: {model.Count}");
+            return View();
+        }   
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPNETCore_HomeTasks_7/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNETCore_HomeTasks_7.Models
+{
+    public class PersonModel
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
+        public int Count { get; set; }
+    }
+}
